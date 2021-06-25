90ba1c61024172e54b81d01f40bc75c64971118b
Tobias Heinrich <tabbe123@googlemail.com>
2021-06-25 14:56:22 +0200

    fetch post titles

---
+++ fetch.py
@@ -0,0 +1,27 @@
+import json
+
+import psaw
+import re
+
+
+def fetch():
+    api = psaw.PushshiftAPI()
+    # there is at least one post missing KotW in the title, therefore we search by selftext.
+    try:
+        with open('data/raw.json') as raw_file:
+            raw_data = json.load(raw_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        raw_data = {}
+    for post in api.search_submissions(subreddit='dominion', author='avocadro', selftext="Welcome to /r/dominion's Kingdom of the Week!"):
+        # posts should be sorted by new, if current post has been previously processed, it is save to assume that there will be no other unprocessed post
+        if post.id in raw_data.keys():
+            break
+        # most titles start with 'KotW \d+/\d+: ', there are only a handful of outliers
+        title = re.sub(r'KotW:? \d+/\d+(?:/\d+)?:? ', '', post.title)
+        raw_data[post.id] = title
+    with open('data/raw.json', 'w') as raw_file:
+        json.dump(raw_data, raw_file)
+
+
+if __name__ == '__main__':
+    fetch()
