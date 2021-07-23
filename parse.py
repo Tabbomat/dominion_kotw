2cf525cabe77f27673c63701509f4cb57291c692
Tobias Heinrich <tabbe123@googlemail.com>
2021-07-23 22:36:01 +0200

    handle current edge cases

--- parse.py
+++ parse.py
@@ -23,10 +23,17 @@ def parse(ids: list = None):
     # except (FileNotFoundError, json.JSONDecodeError):
     #     kotw = {}
     kotw = {}
+
+    def check_type(card_name: str, type_: str) -> bool:
+        card = cards_data.get(card_name)
+        if not card:
+            return False
+        return type_.capitalize() in card['types']
+
     keywords = ('event', 'landmark', 'project', 'colony', 'platinum', 'shelter')
     if not ids:
         ids = raw_data.keys()
-    for post_id in ids:
+    for post_id in sorted(ids):
         if post_id in kotw.keys():
             continue
         title: str = raw_data.get(post_id).lower()
@@ -49,14 +56,21 @@ def parse(ids: list = None):
             title = title.replace(match.group(0), '')
         projects = []
         boons = []
-        bane: Optional[str] = None
         way: Optional[str] = None
         items = [typos.get(item, item) for i in re.split(r'[,.:;]', title) if (item := i.strip(' \r\n\t\\*'))]
         kingdom_cards = []
+
+        def pop_and_check() -> str:
+            _i = items.pop(0)
+            if _i in cards_data.keys():
+                return _i
+            raise ValueError(f"{post_id =}, card = {_i}")
+
         while items:
             # remove spaces
             item = items.pop(0)
             if not item:
+                print("empty")
                 continue
             if len(kingdom_cards) < 10:
                 # current item must be kingdom card
@@ -64,35 +78,60 @@ def parse(ids: list = None):
                 index = item.find('/')
                 if index > 0:
                     item = item[:index]
-                if item in cards_data.keys():
+                if item in cards_data.keys() and cards_data[item]['kingdom']:
                     kingdom_cards.append(item)
                 else:
                     raise ValueError(f"{post_id =}, {item =}")
                 continue
             # at this point, we have events, landmarks, ...
-            if 'colony' in item or 'platin' in item:
+            if item in ('no colony/platinum or shelters', 'no colony/platinum/shelters'):
+                kingdom['colony_platinum'] = False
+                kingdom['shelter'] = False
+            elif item == 'colony/platinum/no shelters':
+                kingdom['colony_platinum'] = True
+                kingdom['shelter'] = False
+            elif 'colony' in item or 'platin' in item:
                 kingdom['colony_platinum'] = 'no ' not in item
-            if 'shelter' in item:
+            elif 'shelter' in item:
                 kingdom['shelter'] = 'no ' not in item
-            if 'event' in item:
+            elif item == 'bane':
+                kingdom['bane'] = pop_and_check()
+            elif re.match(r'events?', item):
                 kingdom['events'] = []
-                while items and not any(k in items[0] for k in keywords) and not items[0] in ('way',):
-                    event = items.pop(0)
-                    if event in cards_data.keys():
-                        kingdom['events'].append(event)
-                    else:
-                        raise ValueError(f"{post_id =}, {event =}")
-            if 'landmark' in item:
+                while items and check_type(items[0], 'Event'):
+                    kingdom['events'].append(pop_and_check())
+            elif re.match(r'landmark?', item):
                 kingdom['landmarks'] = []
-                while items and not any(k in items[0] for k in keywords) and not items[0] in ('way',):
-                    event = items.pop(0)
-                    # fix for obelisk (naming throne room)
-                    event = re.sub(r'\s*\([\w\s]+\)\s*', '', event)
-                    if event in cards_data.keys():
-                        kingdom['landmarks'].append(event)
-                    else:
-                        raise ValueError(f"{post_id =}, {event =}")
-
+                # fix for obelisk (naming throne room)
+                items[0] = re.sub(r'\s*\([\w\s]+\)\s*', '', items[0])
+                while items and check_type(items[0], 'Landmark'):
+                    kingdom['landmarks'].append(pop_and_check())
+                    if items:
+                        items[0] = re.sub(r'\s*\([\w\s]+\)\s*', '', items[0])
+            elif item == 'boons':
+                kingdom['boons'] = []
+                while items and check_type(boon := f"the {items[0]}'s gift", 'Boon'):
+                    kingdom['boons'].append(boon)
+                    items.pop(0)
+            elif item.startswith('project'):
+                kingdom['projects'] = []
+                while items and check_type(items[0], 'Project'):
+                    kingdom['projects'].append(pop_and_check())
+            elif item.startswith('way'):
+                kingdom['ways'] = []
+                while items and check_type(items[0], 'Way'):
+                    kingdom['ways'].append(pop_and_check())
+            elif card := cards_data.get(item):
+                if 'Event' in card['types']:
+                    kingdom.setdefault('events', []).append(item)
+                elif 'Landmark' in card['types']:
+                    kingdom.setdefault('landmarks', []).append(item)
+                elif 'Project' in card['types']:
+                    kingdom.setdefault('projects', []).append(item)
+                else:
+                    print(f"{post_id =}, {item =}")
+            elif item not in ('no events',):
+                print(f"{post_id =}, {item =}")
         kingdom['cards'] = kingdom_cards
         kotw[post_id] = kingdom
 
